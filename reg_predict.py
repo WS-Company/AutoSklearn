7393951181f4d2f871e72f88b6492aa5aee1f2b0
Alex Itkes <38556752+alexitkes@users.noreply.github.com>
2021-09-23 18:45:03 +0300

    Добавлены параметры командной строки --remove-columns и --keep-columns к скрипту reg_predict
    Они пока что не работают и приводят к исключению NotImplementedError

--- reg_predict.py
+++ reg_predict.py
@@ -17,6 +17,17 @@
 
 --quiet, -q
     Подавлять предупреждающие сообщения scikit-learn
+
+--remove-columns СПИСОК
+    Удалить из исходных данных столбцы с указанными номерами. Номера
+    столбцов задаются в формате списка диапазонов или отдельных чисел -
+    например, "1,3,7-9,15-20" удалить первый столбец, третий, с седьмого
+    по девятый и с пятнадцатого по двадцатый включительно.
+
+--keep-columns СПИСОК
+    Удалить из исходных данных все столбцы, кроме столбцов с указанными
+    номерами. Номера задаются также, как и номера столбцов для опции
+    --remove-columns.
 """
 
 import sys
@@ -63,43 +74,62 @@ def run_predictor(model_filename: str,
     sys.stdout.write("{:.5f}\n".format(y[0]))
 
 
-parser = ArgumentParser(
-    usage="--model=ИМЯ_ФАЙЛА --data=ЗНАЧЕНИЕ,ЗНАЧЕНИЕ,...",
-    description="Предсказание на основе модели Scikit-Learn",
-    add_help=True
-)
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        usage="--model=ИМЯ_ФАЙЛА --data=ЗНАЧЕНИЕ,ЗНАЧЕНИЕ,...",
+        description="Предсказание на основе модели Scikit-Learn",
+        add_help=True
+    )
+    parser.add_argument(
+        "--model", "-m",
+        help="Имя файла в который созранена модель Scikit-Learn, с использованием joblib.dump",
+        required=True,
+        type=str,
+        metavar="ИМЯ_ФАЙЛА",
+        dest="model_file"
+    )
+    parser.add_argument(
+        "--data", "-d",
+        help="Строка с разделенными запятыми входными признаками",
+        required=True,
+        type=str,
+        metavar="ЗНАЧЕНИЕ,ЗНАЧЕНИЕ,...",
+        dest="data"
+    )
+    parser.add_argument(
+        "--quiet", "-q",
+        help="Подавлять предупреждения, выводимые Scikit-Learn",
+        action="store_const",
+        const=True,
+        default=False,
+        dest="quiet"
+    )
+    parser.add_argument(
+        "--remove-columns",
+        help="Удалить из данных столбцы с указанными номерами",
+        action="store",
+        type=str,
+        default=None,
+        metavar="COLUMN_LIST",
+        dest="remove_columns"
+    )
 
-parser.add_argument(
-    "--model", "-m",
-    help="Имя файла в который созранена модель Scikit-Learn, с использованием joblib.dump",
-    required=True,
-    type=str,
-    metavar="ИМЯ_ФАЙЛА",
-    dest="model_file"
-)
-
-parser.add_argument(
-    "--data", "-d",
-    help="Строка с разделенными запятыми входными признаками",
-    required=True,
-    type=str,
-    metavar="ЗНАЧЕНИЕ,ЗНАЧЕНИЕ,...",
-    dest="data"
-)
-
-parser.add_argument(
-    "--quiet", "-q",
-    help="Подавлять предупреждения, выводимые Scikit-Learn",
-    action="store_const",
-    const=True,
-    default=False,
-    dest="quiet"
-)
-
-args = parser.parse_args()
-
-run_predictor(
-    model_filename=args.model_file,
-    data=[float(x) for x in args.data.split(",")],
-    quiet=args.quiet
-)
+    parser.add_argument(
+        "--keep-columns",
+        help="Удалить из данных все столбцы кроме указанных",
+        action="store",
+        type=str,
+        default=None,
+        metavar="COLUMN_LIST",
+        dest="keep_columns"
+    )
+    args = parser.parse_args()
+    if args.remove_columns or args.keep_columns:
+        raise NotImplementedError(
+            "Опции --remove-columns и --keep-columns не реализованы"
+        )
+    run_predictor(
+        model_filename=args.model_file,
+        data=[float(x) for x in args.data.split(",")],
+        quiet=args.quiet
+    )
